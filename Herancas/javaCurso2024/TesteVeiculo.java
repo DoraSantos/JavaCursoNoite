0c53075aabc7c9cc535373eb118168107ce7617d
DoraSantos <doras.natura@gmail.com>
2024-10-01 20:45:26 -0300

    Veiculos

---
+++ Herancas/javaCurso2024/TesteVeiculo.java
@@ -0,0 +1,26 @@
+package javaCurso2024;
+
+public class TesteVeiculo {
+	public static void main(String[] args) {
+		Carro carro = new Carro("Toyota","Corrola",2004, 4);
+		System.out.println("Detathes do Carro:");
+		carro.exibirDetalhes();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
